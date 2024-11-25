583194a3f47dbf61903fbd85f33f6ba33bf4386e
Artur <buzov.artur@gmail.com>
2024-11-25 23:49:55 +0100

    sequences

--- src/sequences_and_maps/vector.rs
+++ src/sequences_and_maps/vector.rs
@@ -1,4 +1,10 @@
 fn main() {
+    let xs: [u8; 3] = [1, 2, 3];
+    assert_eq!(xs[0], 1); // index -- usize
+    assert_eq!(xs.len(), 3); // len() -- usize
+    let mut buf = [0u8; 1024]; // a 1024 element buffer filled with zeros
+
+
     // Create vector
     let mut fruits = vec!["apple", "banana", "cherry"];
 
